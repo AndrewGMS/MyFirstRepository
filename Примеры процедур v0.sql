6ab463f5d890fb184d0475ca969f483564ecfdec
Ivan Fool <frsbscrb@rambler.ru>
2021-02-08 14:05:02 +0300

    My SQL commit

--- Примеры процедур v0.sql
+++ Примеры процедур v0.sql
@@ -1,4 +1,6 @@
 -- select * from employees  
 -- call get_accounting_report_labor_code_processing_weekend('2020-11-01', '2020-11-30')
 -- call get_accounting_report_sick_leave_work_hours_for_period('2020-11-01', '2020-11-30')
-call get_accounting_report_students_hours_for_period('2020-11-01', '2020-11-30')
+-- call get_accounting_report_students_hours_for_period('2020-11-01', '2020-11-30')
+-- call timesheetdb.employees_to_payroll_sheet('2020-11-01', '2020-11-30')
+-- SELECT * FROM employees_with_synonyms_of_names

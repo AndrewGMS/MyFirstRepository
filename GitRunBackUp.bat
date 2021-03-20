69f2f82cef568d7c97bc692bfcb9004aa62be76e
Ivan Fool <frsbscrb@rambler.ru>
2021-03-20 15:23:42 +0300

    My SQL commit

--- GitRunBackUp.bat
+++ GitRunBackUp.bat
@@ -2,15 +2,24 @@ y:
 cd \
 cd MySQLFiles 
 git add *.sql
+git status 
+git commit -m "My SQL commit"
+git remote add origin https://github.com/AndrewGMS/MyFirstRepository.git
+git branch -M main
+git push -u origin main
 cd \
 cd MyJavaProjects
-git init
+#git init
 git add *.java
+git status 
+git commit -m "My SQL commit"
+git remote add origin https://github.com/AndrewGMS/MyFirstRepository.git
+git branch -M main
+git push -u origin main
 cd \
 cd MyGitFiles
-git init
+#git init
 git add *.bat
-
 git status 
 git commit -m "My SQL commit"
 git remote add origin https://github.com/AndrewGMS/MyFirstRepository.git

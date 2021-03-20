66dda21debeff062cdf16512437fcfbbff4df7e9
Ivan Fool <frsbscrb@rambler.ru>
2021-03-20 16:11:18 +0300

    My SQL commit

--- GitRunBackUp.bat
+++ GitRunBackUp.bat
@@ -1,12 +1,35 @@
 y:
 cd \
 cd MySQLFiles 
+git init
 git add *.sql
-git status 
 git commit -m "My SQL commit"
 git branch -M main
+git pull
 git push -u origin main
-msg awk "Git SQL Scripts BkUp may be done"
+
+
+cd \
+cd MyJavaProjects
+git init
+git add *.java
+git branch -M JavaSand
+git pull
+git push -u origin JavaSand
+
 
 cd \
 cd MyGitFiles
+git init
+git add *.bat
+git status 
+git commit -m "My SQL commit"
+git branch -M Bat
+git pull
+git push -u origin Bat
+msg awk "Git SQL Scripts BkUp may be done"
+
+
+cd \
+cd MyGitFiles
+

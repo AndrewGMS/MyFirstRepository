598b06f4601e5342df93de9e698f48cd479e4289
Ivan Fool <frsbscrb@rambler.ru>
2022-08-25 14:05:01 +0300

    My SQL commit

---
+++ FillDataCompanies.sql
@@ -0,0 +1,42 @@
+
+
+
+
+/*
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 1, 4, 'ООО "Дот альфа"');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 2, 4, '0');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 3, 4, 'ДА');
+
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 1, 1, 'ООО "ММТР"');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 2, 1, '1');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 3, 1, 'ММТР');
+
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 1, 2, 'ООО "ММТР технологии"');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 2, 2, '1');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 3, 2, 'ММТР технологии');
+
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 1, 3, 'ООО "СофтЭкспорт"');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 2, 3, '1');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 3, 3, 'СофтЭкспорт');
+
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 1, 4, 'ООО "Дот альфа"');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 2, 4, '0');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 3, 4, 'ДА');
+
+
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 1, 5, 'ООО "ТЕСТ_01 нет параметра дебиторка"');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 2, 5, '0');
+
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 1, 6, 'ООО "ТЕСТ_02 параметр дебиторка пустая строка"');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 2, 6, '0');
+insert into managementreport.maindictionary (StructureId, UnitId, DataId, DataValue) values (2, 3, 6, '');
+
+
+
+
+2	Организации. Юридические лица.	0	0	
+2	Наименование Организации	1	0	
+2	Бизнес единица.	2	1	0
+2	Ключ. Дебиторка	3	0	
+
+*/

5c6aff846ea7b6e881a6b456cdc74b052e933a32
Ivan Fool <frsbscrb@rambler.ru>
2022-09-06 14:05:01 +0300

    My SQL commit

--- InsertUpdateAccBalance.sql
+++ InsertUpdateAccBalance.sql
@@ -1,20 +1,52 @@
-
+/*
 Set @DebDateOperaton = '2022-08-01';
 Set @DebCompanyName = 'ММТР';
 Set @DebBankName = 'ВТБ';
 Set @DebSumOper = 1663737.92;
+*/
+-- select * from managementreport.debcompanyandbank
+-- select * from managementreport.maindictionary
+
+Set @DebDateOperaton = '2022-08-02';
+Set @DebCompanyName = 'ММТР';
+Set @DebBankName = 'ВТБ';
+Set @DebSumOper = 704970.91;
 
 call InsertUpdateAccBalance(@DebDateOperaton, @DebCompanyName, @DebBankName, @DebSumOper);
 
+select SettlementBalance(@DebDateOperaton, 4, 1, 1);
+-- select date_sub(@DebDateOperaton, interval 1 day)
+
+/*
+select SettlementBalance(@DebDateOperaton, 4, 1, 1)
+
 Set @TestData = (select count(AccountName) from managementreport.debcompanyandbank DCB where DCB.DebCompanyName = @DebCompanyName and DCB.DebBankName = @DebBankName);
 
-if @TestData = 0
+select * from managementreport.debcompanyandbank DCB where DCB.DebCompanyName = @DebCompanyName and DCB.DebBankName = @DebBankName
+
+
+select ifnull(sum(AMR.TransactionAmount),0) as xSum, 'Дебетовый оборот' as xValue
+from managementreport.AccumulationAndMovementRegister AMR
+       where AMR.DebitStructureId = 4
+       and AMR.DebitUnitId = 1
+       and AMR.DebitDataId = 1
+       and AMR.DateOperation <= @DebDateOperaton
+union all
+select ifnull(-sum(AMR.TransactionAmount),0) as xSum, 'Кредитовый оборот' as xValue 
+from managementreport.AccumulationAndMovementRegister AMR
+where AMR.DebitStructureId = 4 
+       and AMR.DebitUnitId = 1
+       and AMR.DebitDataId = 1
+       and AMR.DateOperation <= @DebDateOperaton
+
+
+if (@TestData = 0)
 then
 
--- select @TestData
+select @TestData
 else 
 
--- select @TestData+100
+select @TestData+100
 
 end if;
 
@@ -23,7 +55,7 @@ end if;
 -- select if (@TestData = 0, @TestData, @TestData)
 
 
-/*
+
 select @TestData, AccountName from managementreport.debcompanyandbank DCB ,
 
 select @TestData, AccountName from managementreport.debcompanyandbank DCB

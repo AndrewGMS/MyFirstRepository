b3b8271a416723b3886d77977f77a3a8ef38cdf1
Ivan Fool <frsbscrb@rambler.ru>
2022-10-04 14:05:01 +0300

    My SQL commit

---
+++ SoftlineTimeSheet.sql
@@ -0,0 +1,7 @@
+call bench_data_to_softline('2022-09-01', '2022-09-22');
+
+-- call timesheetdb.employees_to_payroll_sheet('2022-09-01', '2022-09-22');
+call timesheet_to_softline('2022-08-01', '2022-08-31');
+call accounting_to_softline_1('2022-08-01', '2022-08-31');
+call accounting_to_softline_2('2022-08-01', '2022-08-31');
+call accounting_to_softline_3('2022-08-01', '2022-08-31');

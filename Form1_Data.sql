ea77d89b162c19502eee81b9ba1fbce15bc194fc
Ivan Fool <frsbscrb@rambler.ru>
2022-11-10 14:05:01 +0300

    My SQL commit

--- Form1_Data.sql
+++ Form1_Data.sql
@@ -8,8 +8,37 @@ and not (xClientId = 5 and xProgramId = 106 and xProjectId = 100 and xWorkType l
 and not (xClientId = 5 and xProgramId = 106 and xProjectId = 100 and xWorkType like '%Отгул%') 
 and not (xClientId = 5 and xProgramId = 106 and xProjectId = 100 and xWorkType like '%Больничный%')
 and not (xClientId = 5 and xProgramId = 106 and xProjectId = 100 and xWorkType like '%Отпуск%')
-and (xClientId = 5 and xProgramId = 37 and xProjectId = 3800 )
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 3800 )  -- 303 Управление персоналом
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 3801 )  -- 306 Бухгалтерия
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 3804 )  -- 305 АХО
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 5714 ) -- 'Develonica-needs' PR
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 3799  ) -- 'Внутренние отделы и проекты' PR
+and not (xClientId = 5 and xProgramId = 298 and xProjectId = 4  ) -- MyMMTR
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 4614) -- обучение дизайнеров
+and not (xClientId = 5 and xProgramId = 234 and xProjectId = 5558   ) -- английский
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 3802   ) -- 302 Управление производством ЕСТЬ ВОПРОСЫ!!!
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 3468   ) -- 307 сисадмины
+and not (xClientId = 5 and xProgramId = 234 and xProjectId = 484  ) -- 308 отдел продаж
+and not (xClientId = 5 and xProgramId = 234 and xProjectId = 1627  ) -- Преподавание
+and not (xClientId = 5 and xProgramId = 234 and xProjectId = 284  ) -- Наставничество
+and not (xClientId = 5 and xProgramId = 241 and xProjectId = 4040  ) -- Camunda
+and not (xClientId = 5 and xProgramId = 234 and xProjectId = 3806  ) -- 304 Управление ресурсами
+and not (xClientId = 5 and xProgramId = 234 and xProjectId = 3333  ) -- 308 отдел продаж
+and not (xClientId = 5 and xProgramId = 317 and xProjectId = 5908 ) -- HR страница
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 4317  ) -- 304 Управление ресурсами
+and not (xClientId = 5 and xProgramId = 310 and xProjectId = 3795  ) -- ЛАФ
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 4125  ) -- 304 Управление ресурсами
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 4094  ) -- 304 Управление ресурсами
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 4437 )  -- 304 Управление ресурсами
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 4717 )  -- 304 Управление ресурсами
+and not (xClientId = 5 and xProgramId = 37 and xProjectId = 51 )  -- 304 Управление ресурсами
 
+-- and ( xWorkType like '%Наставничество и обучение%' ) -- обучение дизайнеров
+
+
+
+
+order by xSpecialist
 
 /*
 set @Bdate = '2022-08-01';
---
+++ Вспомогательные таблицы.sql
@@ -0,0 +1,106 @@
+use brokenboeing;
+
+
+
+/*
+create table brokenboeing.TypeOfEmploymentTable(
+tId decimal(10),
+tDescription varchar(200)
+);
+
+
+
+insert into brokenboeing.TypeOfEmploymentTable(tId, tDescription) values (1001,'Работа на проекте');
+insert into brokenboeing.TypeOfEmploymentTable(tId, tDescription) values (1002,'Перереботка');
+insert into brokenboeing.TypeOfEmploymentTable(tId, tDescription) values (1003,'Отсутствие занятости');
+
+select * from brokenboeing.TypeOfEmploymentTable;
+
+*/
+/*
+create table brokenboeing.ProjectPhaseTable(
+tId decimal(10),
+tDescription varchar(200)
+);
+
+
+
+insert into brokenboeing.ProjectPhaseTable(tId, tDescription) values (901,'Пресейл');
+insert into brokenboeing.ProjectPhaseTable(tId, tDescription) values (902,'Работы по созданию продукта');
+insert into brokenboeing.ProjectPhaseTable(tId, tDescription) values (903,'Гарантийные работы');
+
+select * from brokenboeing.ProjectPhaseTable;
+*/
+/*
+create table brokenboeing.ProjectPropertyTable(
+tId decimal(10),
+tDescription varchar(200),
+tActivity_ID decimal(10),
+tSponsor_ID decimal(10),
+tCustomer_ID decimal(10),
+tBDate date,
+tEDate date
+);
+
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID) values ('801', 'Внешний проект (произвольное название)', '501', '201', '301');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID) values ('802', 'Внутренний проект (произвольное название)', '501', '202', '302');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('803', 'Больничный', '502', '202', '304', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('804', 'Ежегодный отпуск', '502', '202', '304', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('805', 'Отпуск без сохранения заработной платы', '502', '202', '304', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('806', 'Организационные задачи', '502', '202', '304', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('807', 'Поиск и собеседование кандидатов', '502', '202', '303', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('808', 'Развитие компетенций', '502', '202', '304', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('809', 'Стажировка', '502', '202', '304', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('810', 'Наставничество', '502', '202', '304', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('811', 'Оплачиваемый выходной', '502', '202', '303', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('812', 'Копроративные мероприятия', '502', '202', '303', '2022-04-01', '2023-03-31');
+insert into brokenboeing.ProjectPropertyTable (tId, tDescription, tActivity_ID, tSponsor_ID, tCustomer_ID, tBDate, tEDate) values ('813', 'Отсутствие занятности', '502', '202', '304', '2022-04-01', '2023-03-31');
+
+
+
+select * from ProjectPropertyTable; 
+
+
+-- ID	Наименование	Спр_Тип Активности_ID	Спр_Спонсор_ID	Спр_Название заказчика_ID	Дата начала проекта	Дата окончания проекта
+
+*/
+/*
+create table brokenboeing.PaymentModelTable(
+tId decimal(10),
+tDescription varchar(200)
+);
+
+
+
+insert into brokenboeing.PaymentModelTable(tId, tDescription) values (701,'Time & Material');
+insert into brokenboeing.PaymentModelTable(tId, tDescription) values (702,'Fixed Price');
+insert into brokenboeing.PaymentModelTable(tId, tDescription) values (703, 'Full Time');
+insert into brokenboeing.PaymentModelTable(tId, tDescription) values (704, 'Outstaffing');
+
+select * from brokenboeing.PaymentModelTable;
+*/
+/*
+create table brokenboeing.ProjectClassTable(
+tId decimal(10),
+tDescription varchar(200)
+);
+
+delete from brokenboeing.ProjectClassTable where tId = 601;
+delete from brokenboeing.ProjectClassTable where tId = 602;
+insert into brokenboeing.ProjectClassTable(tId, tDescription) values (601, 'Операционный');
+insert into brokenboeing.ProjectClassTable(tId, tDescription) values (602, 'Инвестиционный');
+
+select * from brokenboeing.ProjectClassTable;
+*/
+/*
+create table brokenboeing.ActivityTable(
+tId decimal(10),
+tDescription varchar(200)
+)
+
+insert into brokenboeing.ActivityTable(tId, tDescription) values (501, 'Активности производственные');
+insert into brokenboeing.ActivityTable(tId, tDescription) values (502, 'Активности Активность непроизводственная');
+--  501	Активности производственные 502	Активность не производственная 
+
+select * from brokenboeing.ActivityTable
+*/

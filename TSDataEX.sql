f04f068e38673e572c8376386d966487b8271071
Ivan Fool <frsbscrb@rambler.ru>
2022-11-12 14:05:01 +0300

    My SQL commit

--- TSDataEX.sql
+++ TSDataEX.sql
@@ -1,8 +1,90 @@
 
+insert into brokenboeing.TimeSheetConnectionTable (
+   xClientId 
+ , xProgramId
+ , xProjectId
+ , xWorkType
+ , xProjectResult_Id
+ 
+ , projectpropertytable_Id
+ , customertypetable_Id) 
+ values (
+ 5 -- xClientId 
+ , 106 -- xProgramId
+ , 100 -- xProjectId
+ ,'%Нет задач%' --  xWorkType
+ , 0 -- xProjectResult_Id
+ , 813 --  projectpropertytable_Id
+ , 304 -- customertypetable_Id
+ )
 
--- SELECT * FROM brokenboeing.timesheetex where xComment like '%''%';
+select * from brokenboeing.TimeSheetConnectionTable;
+/*
+drop table brokenboeing.TimeSheetConnectionTable;
+
+create table brokenboeing.TimeSheetConnectionTable (
+
+
+   xbDate timestamp default '2022-01-01'
+ , xeDate timestamp default '2023-03-31'
+ , xClientId varchar(10)
+ , xProgramId varchar(10)
+ , xProjectId varchar(10)
+ , xWorkType varchar(100)
+ , bustructuretable_Id varchar(10) default '0' -- Спр_ЮЛ_Наименование БЕ, больше чем уверен что здесь не нужно, надо привязывать к проекту
+ , xProjectResult_Id varchar(10)
+ , projectpropertytable_Id varchar(10)
+ , customertypetable_Id varchar(10)
+ 
+ );
+
+*/
+
+/*SELECT 
+--   xDate
+-- , xEmloeeName
+-- , xEmploeeNameShort
+-- ,
+ xCustomerName
+, xProgrammName
+, xProjectName
+, xWorkType
+ , xSpecialist
+-- , xIsBusinessTrip
+, sum(xTimeSheetWorkHours) as xTimeSheetWorkHours
+, sum(xTimeSheetOverWorkHours) as xTimeSheetOverWorkHours
+-- , xIsInnerOverWork
+-- , xPrice
+-- , xCost
+, sum(xCost) as xCost
+-- , xComment
+-- , xTypeOfProblem
+, xClientId
+, xProgramId
+, xProjectId
+-- , xWorkRecordId
+-- , xUserId
+-- , xDateModified
+FROM brokenboeing.timesheetex tsex
+where xClientId = 4
+and xWorkType like '%'
+and xDate between '2022-08-01' and '2022-08-31'
+-- and xProgramId like '20'
+group by 
+ xCustomerName
+, xProgrammName
+, xProjectName
+, xWorkType
+, xClientId
+, xProgramId
+, xProjectId
+ , xSpecialist
+-- , xTypeOfProblem
+-- where xComment like '%''%';
 
 -- delete FROM brokenboeing.timesheetex;
+
+*/
 /*
 drop table brokenboeing.TimeSheetEx;
 

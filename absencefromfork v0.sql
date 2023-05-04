ff757c4b487d7c7a40ca4f79a63168ebd47e6fbb
Ivan Fool <frsbscrb@rambler.ru>
2023-05-04 14:05:00 +0300

    My SQL commit

--- absencefromfork v0.sql
+++ absencefromfork v0.sql
@@ -1,7 +1,8 @@
+call brokenboeing.GetAbsenceFromWork('2023-05-01');
 
 -- call setabsencefromwork ('ООО «ММТР технологии»', '4401185451', '00000379', '2023-04-28', 'Отпуск очередной', '104', 'Ежегодный основной оплачиваемый отпуск', '09', '1', '440120558001', 'Баукин Кирилл Георгиевич', '2023-05-05', '2023-05-05', 'Нет', 'Да');
 
- select * from brokenboeing.absencefromfork where CompanyTaxNumber = '4401069776' order by NumDoc;
+-- select * from brokenboeing.absencefromfork where CompanyTaxNumber = '4401069776' order by NumDoc;
 /*
 SET SQL_SAFE_UPDATES = 0; delete from brokenboeing.absencefromfork; SET SQL_SAFE_UPDATES = 1;
 drop table brokenboeing.absencefromfork;

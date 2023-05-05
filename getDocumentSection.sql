7f0446536df6e12d25fd463c9b84ecf21ce34559
Ivan Fool <frsbscrb@rambler.ru>
2023-05-05 14:05:00 +0300

    My SQL commit

---
+++ getDocumentSection.sql
@@ -0,0 +1,27 @@
+-- SELECT * FROM managementreport.maindocumentstructure where DocumentStructureID = 3 ;
+set @pDocumentStructureID= 3;
+set @pDataSectionId =  0;
+set @pDocumentId =  555;
+
+
+select 
+ mds.DocumentStructureID
+ , mds.DataSectionId
+ , mds.DataTableIndexId
+ , mds.DataId
+ , mds.DataDefaultValue
+ , mds.DictionarytStructureId
+ , mds.DocumentUnitName
+ , mds.DocumentUnitCaption
+ , ifnull(mdd.DocumentId, @pDocumentId) 
+ , ifnull(mdd.DataValue, mds.DataDefaultValue)
+ from managementreport.maindocumentstructure mds 
+ left join managementreport.maindocumentdata mdd 
+      on mdd.DocumentStructureID = mds.DocumentStructureID 
+         and mdd.DataSectionId = mds.DataSectionId
+         and mdd.DataId = mds.DataId 
+         and mdd.DocumentId = @pDocumentId
+      
+ where mds.DocumentStructureID = @pDocumentStructureID
+   and mds.DataSectionId = @pDataSectionId
+ ;

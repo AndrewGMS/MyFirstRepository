75ceb0b0ba51e6b8bf5b87f76ebdba5409f4131a
Ivan Fool <frsbscrb@rambler.ru>
2023-07-26 14:05:04 +0300

    My SQL commit

--- GitFront.bat
+++ GitFront.bat
@@ -1,9 +1,27 @@
 cd \
 y:
 cd y:\OpenServer\domains\Main\ 
+
+REM PUSH
+
 git add *.*
 git commit -m "front%Date:~6,4%%Date:~3,2%%Date:~0,2%%Time:~0,2%%Time:~3,2%%Time:~6,2%"
-git status
+REM git status
 REM git remote add origin https://github.com/AndrewGMS/account.git
 git branch -M front
 git push -u origin front
+
+REM PULL
+
+REM git add *.*
+REM git commit -m "front%Date:~6,4%%Date:~3,2%%Date:~0,2%%Time:~0,2%%Time:~3,2%%Time:~6,2%"
+REM git status
+REM git remote add origin https://github.com/AndrewGMS/account.git
+git branch -M front
+git pull https://github.com/AndrewGMS/account.git
+
+
+cd c:\MyGitFiles\
+
+
+msg awk "Git Front Scripts BkUp may be done"
---
+++ GitFrontPull.bat
@@ -0,0 +1,10 @@
+cd \
+y:
+cd y:\OpenServer\domains\Main\ 
+REM git add *.*
+REM git commit -m "front%Date:~6,4%%Date:~3,2%%Date:~0,2%%Time:~0,2%%Time:~3,2%%Time:~6,2%"
+REM git status
+REM git remote add origin https://github.com/AndrewGMS/account.git
+git branch -M front
+git pull https://github.com/AndrewGMS/account.git
+cd c:\MyGitFiles\
